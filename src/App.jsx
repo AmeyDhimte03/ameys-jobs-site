97a5872d1632bca4415c1e12efde86558acc76ca
Amey Dhimte <ameydhimte03@gmail.com>
2024-05-21 18:29:40 +0530

    Added all the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Navbar from "./components/navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 import HomeCards from "./components/HomeCards.jsx";
+import JobListings from "./components/JobListings.jsx";
+import ViewAllJobs from "./components/ViewAllJobs.jsx";
 
 const App = () => {
   return (
     <>
       <Navbar/>
+      <Hero/>
       <HomeCards/>
+      <JobListings/>
+      <ViewAllJobs/>
     </>
   );
 };
